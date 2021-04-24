f0479aff7c17221af3799f76ef292cf9d595250e
andaraGui <guiandaradev@gmail.com>
2021-04-24 11:51:41 -0300

    Revert "Merge branch 'main' of https://github.com/andaraGui/URI_JUDGE_JS into main"
    This reverts commit ac2d95ff9196a720c5354aaa39b3a53e177a79e7, reversing
    changes made to 1816478345cb3adfeddba1de59b938e1431e21ba.

--- URI_1004.js
+++ URI_1004.js
@@ -1,6 +1,6 @@
 /*
-Read two integer values. After this, 
-calculate the product between them and store the result in a variable named PROD. 
+Read two integer values. After this, c
+alculate the product between them and store the result in a variable named PROD. 
 Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
 
 Input
