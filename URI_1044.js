301b5ddeac63a7e1301f809a5e3daf74dc80a115
andaraGui <guiandaradev@gmail.com>
2021-05-03 13:12:38 -0300

    03/05

--- URI_1044.js
+++ URI_1044.js
@@ -1,3 +1,12 @@
+/*
+Read two nteger values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+Input
+The input has two integer numbers.
+
+Output
+Print the relative message to the input as stated above. 
+*/
 var input = require('fs').readFileSync('URI_1044_stdin', 'utf8');
 var lines = input.split(' ').map(Number);
 if(lines[1] % lines[0] === 0 || lines[0] % lines[1] === 0){
