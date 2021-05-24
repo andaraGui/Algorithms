bd36bfda260ecf95b2e833d2449c46a9953ff0a8
andaraGui <guiandaradev@gmail.com>
2021-05-24 17:32:55 -0300

    24/05

--- URI_1186.js
+++ URI_1186.js
@@ -2,8 +2,6 @@
 Read an uppercase character that indicates an operation that will be performed in an array M[12][12]. Then, calculate and print the sum or average considering only that numbers that are below of the Secundary diagonal of the array, like shown in the following figure (green area).
 
 
-
-
 Input
 The first line of the input contains a single uppercase character O ('S' or 'M'), indicating the operation Sum or Average (Média in portuguese) to be performed with the elements of the array. Follow 144 floating-point numbers of the array.
 
