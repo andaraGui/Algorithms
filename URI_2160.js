09856a61493a33c55f0b4295924e9178b964d495
andaraGui <guiandaradev@gmail.com>
2021-06-11 13:24:14 -0300

    commit

--- URI_2160.js
+++ URI_2160.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('URI_2160_stdin', 'utf8')
 const inputLines = input.split('\r')
 
-if(inputLines[0].length <= 81){
+if(inputLines[0].length <= 81){//why 81? i don't know
     console.log('YES');
 }else{
     console.log('NO');
