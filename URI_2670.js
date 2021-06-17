0062b463fc21493989174d5892ef8c21a95b6d4a
andaraGui <guiandaradev@gmail.com>
2021-06-17 13:28:04 -0300

    commit

---
+++ URI_2670.js
@@ -0,0 +1,24 @@
+const input = require('fs').readFileSync('URI_2670_stdin','utf-8');
+const employeesByFloor = input.split('\n').map(Number);
+
+const buildingFloors = 3;
+
+let minsExpendArray = new Array(buildingFloors).fill(0);
+
+for(let  i = 0 ; i < buildingFloors ; i++){
+
+    for(let j = 0 ; j < buildingFloors ; j++){
+        if(j !== i){
+            
+            let floorsDiference = j - i;
+            if(floorsDiference < 0){
+                floorsDiference = -floorsDiference;
+            }
+
+            minsExpendArray[i] += employeesByFloor[j] * 2 * floorsDiference; 
+        }
+    }
+}  
+minsExpendArray = minsExpendArray.sort((a, b) => a - b)
+console.log(minsExpendArray[0])
+
